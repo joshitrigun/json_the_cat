2f6a8cbf787ae7b674b7b9e276f2bc8b38fa2613
Trigun Joshi <trigunjoshi@gmail.com>
2021-12-02 17:34:20 -0800

    test added and breedFetcher function

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,21 +1,23 @@
 const request = require("request");
-const userInput = process.argv[2];
+//const userInput = process.argv[2];
 // console.log(userInput);
-//const fetchBreedDescription = (breedName, cb) => {
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${userInput}`;
-request(url, (error, res, body) => {
-  if (error) {
-    console.log(`Failed to request details: ${error}`, null);
-  }
+const fetchBreedDescription = (breedName, cb) => {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(url, (error, res, body) => {
+    if (error) {
+      cb(`Failed to request details: ${error}`, null);
+    }
+    //console.log(res)
+    const data = JSON.parse(body);
+    //console.log(data);
+    const breed = data[0];
+    // console.log(breed);
+    if (breed) {
+      cb(null, breed.description);
+    } else {
+      cb(`Failed to find breed ${userInput}`, null);
+    }
+  });
+};
 
-  const data = JSON.parse(body);
-  //console.log(data);
-  const breed = data[0];
-  // console.log(breed);
-  if (breed) {
-    console.log(breed.description);
-  } else {
-    console.log(`Failed to find breed ${userInput}`, null);
-  }
-});
-//};
+module.exports = { fetchBreedDescription };
